253c69e0c10910987a7a25afae914d8f4e327a8b
chris5802 <54934948+chris5802@users.noreply.github.com>
2021-06-09 22:11:54 +0800

    增加樓梯種類(滑輪樓梯)

--- FinalProject.asm
+++ FinalProject.asm
@@ -33,6 +33,7 @@ VK_RIGHT EQU 000000027h  ;右方向鍵
 arr byte 0,0,0,0,0,0,0,0,0,0				;地圖陣列
 arr_y byte 0,3,6,9,12,15,18,21,24,27		;地圖陣列之y座標紀錄
 arr_x byte 0,0,0,0,0,0,0,0,0,0				;地圖陣列之x座標紀錄
+arr_type byte 0,0,0,0,0,0,0,0,0,0			;0:一般地板 1:Sham 2:尖刺 3:滾動
 
 top DWORD 0
 bottom DWORD 9
@@ -70,7 +71,14 @@ Player2 EQU 'B'
 Score2 DWORD 0								;角色2分數
 
 Wall EQU '|'				;牆壁
-Floor EQU 'TTTTTTTTTT'
+Floor EQU 'TTTTTTTTTT'		
+Sham  EQU 'HHHHHHHHHH'		;踩到一秒後掉落
+Spike EQU 'AAAAAAAAAA'		;踩到後死亡
+RollRight_1 EQU '->->->->->'
+RollRight_2 EQU '>->->->->-'
+RollLeft_1 EQU '-<-<-<-<-<'
+RollLeft_2 EQU '<-<-<-<-<-'
+RollState Byte 0
 Empty EQU '          '		
 
 Min_X EQU 30			
@@ -79,8 +87,10 @@ Min_Y EQU 0
 Max_Y EQU 30			;地圖高
 
 flag DWORD 0
-State1 BYTE 0          ; 0:不在樓梯上，1:在樓梯上，2:死掉
+State1 BYTE 0          ; 0:不在樓梯上，1:在樓梯上，2:死掉，3:跌在其他人身上
 State2 BYTE 0
+Floor_type1 byte ?
+Floor_type2 byte ?
 
 .code	;this is the code area
 
@@ -94,8 +104,10 @@ main proc
 		mov eax , 6
         call RandomRange
         mov arr[esi] , al
+		mov eax , 5
+        call RandomRange
+        mov arr_type[esi] , al
 		inc esi
-		
 	loop FloorInitial
 
 	call ShowWall
@@ -165,6 +177,7 @@ ShowRole  PROC
 			.IF State2!=2
 				inc Score2
 			.ENDIF
+			call Left_Right
 			call ShowFloor
 			call ChangeState
 			INVOKE GetTickCount        ; get starting tick count
@@ -237,11 +250,16 @@ ShowFloor PROC USES eax
 		L1:
 			mov al,arr[esi+1]
 			mov arr[esi],al
+			mov al,arr_type[esi+1]
+			mov arr_type[esi],al
 			inc esi
 		Loop L1
 		mov eax , 6 
         call RandomRange
 		mov arr[9],al
+		mov eax , 5 
+        call RandomRange
+		mov arr_type[9],al
         
 	.ELSEIF
 		dec FloorCount
@@ -255,15 +273,15 @@ ShowFloor PROC USES eax
 	
 
 	.IF State1 == 1 || State1 == 3		;on the stair
-		;mGoto CurrentPos_X1, CurrentPos_Y1
-		;mWrite ' '
-		;dec CurrentPos_Y1
+		mGoto CurrentPos_X1, CurrentPos_Y1
+		mWrite ' '
+		dec CurrentPos_Y1
 	.ENDIF
 
 	.IF State2 == 1 || State2 == 3
-		;mGoto CurrentPos_X2, CurrentPos_Y2
-		;mWrite ' '
-		;dec CurrentPos_Y2
+		mGoto CurrentPos_X2, CurrentPos_Y2
+		mWrite ' '
+		dec CurrentPos_Y2
 	.ENDIF
 	
 
@@ -279,13 +297,37 @@ ShowFloor PROC USES eax
 
 		.IF FloorCount!=0 || esi !=0
 			mGoto al,CurrentFloor
-			mWrite Floor
+			.IF arr_type[esi]==0
+				mWrite Floor
+			.ELSEIF arr_type[esi]==1
+				mWrite Sham
+			.ELSEIF arr_type[esi]==2
+				mWrite Spike
+			.ELSEIF arr_type[esi]==3
+				.IF RollState==0
+					mWrite RollRight_1
+				.ELSE 
+					mWrite RollRight_2
+				.ENDIF
+			.ELSEIF arr_type[esi]==4
+				.IF RollState==0
+					mWrite RollLeft_1
+				.ELSE 
+					mWrite RollLeft_2
+				.ENDIF
+			.ENDIF
 		.ENDIF
 		add CurrentFloor,3
 		inc esi
 
 	.IF esi<10
 		JMP FloorLoop
+	.ENDIF
+
+	.IF RollState==0
+		mov RollState,1
+	.ELSE 
+		mov RollState,0
 	.ENDIF
 
 		ret
@@ -312,8 +354,8 @@ ShowDrop PROC USES eax
 			inc CurrentPos_Y2
 		.ENDIF		
 		
-		mov eax,0
-		mov flag,eax
+		
+		
 		
 		ret
 	ShowDrop EndP
@@ -334,6 +376,7 @@ ShowScore PROC USES eax
 ;----------------------------------------
 ShowWall PROC	USES eax
 	
+
 	mov al,Min_Y
 	WallLoop:
 		
@@ -361,7 +404,25 @@ ShowWall PROC	USES eax
 		mov ah,CurrentFloor
 		mov arr_y[esi],ah
 		mGoto al,CurrentFloor
-		mWrite Floor
+		.IF arr_type[esi]==0
+			mWrite Floor
+		.ELSEIF arr_type[esi]==1
+			mWrite Sham
+		.ELSEIF arr_type[esi]==2
+			mWrite Spike
+		.ELSEIF arr_type[esi]==3
+			.IF RollState==0
+				mWrite RollRight_1
+			.ELSE 
+				mWrite RollRight_2
+			.ENDIF
+		.ELSEIF arr_type[esi]==4
+			.IF RollState==0
+				mWrite RollLeft_1
+			.ELSE 
+				mWrite RollLeft_2
+			.ENDIF
+		.ENDIF
 		add CurrentFloor,3
 		inc esi
 	.IF esi<10
@@ -376,11 +437,14 @@ ShowWall PROC	USES eax
 		inc al
 	Loop SpikeLoop
 
+	mov RollState,1
+
 	mGoTo 1,1
 	mWrite 'player1 score:'
 	mGoTo 93,1
 	mWrite 'player2 score:'
 
+	
 		
 	ret
 ShowWall ENDP
@@ -637,5 +701,73 @@ S2:
 E1:	
 	ret
 ChangeState endp
+;---------------------------------------------
+;					left or right
+;---------------------------------------------
 
+Left_Right proc  
+
+ mov Floor_type1,0
+ mov bl, CurrentPos_Y1
+ mov bh, CurrentPos_X1
+ inc bl
+ mov esi, 0
+ L1:
+	.IF bl == arr_y[esi] || esi>9
+		.IF arr_type[esi]==3 || arr_type[esi]==4
+			mov al,arr_type[esi]
+			mov Floor_type1,al
+		.ENDIF
+	    jmp EndL1
+	.ELSE
+	    inc esi
+		jmp L1
+	.ENDIF
+ EndL1:
+
+ .IF State1 == 1 
+     .IF Floor_type1 == 3
+	    .IF CurrentPos_X2 <90
+			inc CurrentPos_X1
+		.ENDIF
+	 .ELSEIF Floor_type1 == 4
+	     .IF CurrentPos_X2 > 31
+			dec CurrentPos_X1
+		.ENDIF
+	 .ENDIF
+	 
+ .ENDIF
+
+ mov Floor_type2,0
+ mov bl, CurrentPos_Y2
+ mov bh, CurrentPos_X2
+ inc bl
+ mov esi, 0
+ L2:
+	.IF bl == arr_y[esi] || esi>9
+		.IF arr_type[esi]==3 || arr_type[esi]==4
+			mov al,arr_type[esi]
+			mov Floor_type1,al
+		.ENDIF
+	    jmp EndL2
+	.ELSE
+	    inc esi
+		jmp L2
+	.ENDIF
+ EndL2:
+
+ .IF State2 == 1 
+     .IF Floor_type1 == 3
+		.IF CurrentPos_X2<90
+			inc CurrentPos_X2
+		.ENDIF
+	 .ELSEIF Floor_type1 == 4
+		.IF CurrentPos_X2>31
+			dec CurrentPos_X2
+		.ENDIF
+	 .ENDIF
+	 
+ .ENDIF
+	ret
+Left_Right endp
 end main
